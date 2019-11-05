2eb296384c42da98f2ac1c8e7287c9de81e39d2b
Vasin Srisupavanich <mvsmark@gmail.com>
2019-11-05 15:48:04 +0000

    Add game objects

--- BlocksWorld.py
+++ BlocksWorld.py
@@ -1,25 +1,37 @@
-
 class BlocksWorld:
+    """
+       Blocksworld tile puzzle as described in the AI coursework
+       The puzzle can be of any N x N size depends on the input.
+       This class defines the mechanics of the puzzle.
+       The following are what represent each object in the game.
+       X represents an agent.
+       Empty space represents empty tile.
+       Alphabet letter (A, B, C) represents the block.
+       The goal of this game is to arrange the block according to the rule (goal state).
+       """
 
-    def __init__(self, size):
+    def __init__(self, size, start_state):
         self.size = size
-        self.board = [[0 for x in range(size)] for x in range(size)]
-        for i in range(size):
-            for j in range(size):
-                self.board[i][j] = ' '
-        
+        self.board = [['' for x in range(size)] for x in range(size)]
+        for row in range(size):
+            for col in range(size):
+                self.board[row][col] = start_state.get(size * row + col + 1, ' ')
+
+    def is_goal(self):
+        return True
+
     def create_board_string(self):
         lines = []
+        vertical_line = '|'
         horizontal_line = '-' * (self.size * 4 + 1)
-        lines.append(horizontal_line)
         for row in range(self.size):
-            row_line = '|'
+            row_line = ''
             for col in range(self.size):
-                row_line = row_line + ' ' + self.board[col][row] + ' |'
-            lines.append(row_line)
+                row_line += vertical_line + ' ' + str(self.board[row][col]) + ' '
             lines.append(horizontal_line)
+            lines.append(row_line + vertical_line)
+        lines.append(horizontal_line)
         return '\n'.join(lines)
 
     def display_board(self):
         print(self.create_board_string())
-
--- Runner.py
+++ Runner.py
@@ -1,5 +1,11 @@
 from BlocksWorld import BlocksWorld
 
-world = BlocksWorld(4)
+start_state = {
+    13: "A",
+    14: "B",
+    15: "C",
+    16: "X"
+}
+world = BlocksWorld(4, start_state)
 
 world.display_board()

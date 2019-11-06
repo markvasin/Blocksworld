22c050d3c983f9c33308abe883a94d71d844bcc6
Vasin Srisupavanich <mvsmark@gmail.com>
2019-11-06 00:44:50 +0000

    Complete 1 player game

--- BlocksWorld.py
+++ BlocksWorld.py
@@ -16,9 +16,48 @@ class BlocksWorld:
         for row in range(size):
             for col in range(size):
                 self.board[row][col] = start_state.get(size * row + col + 1, ' ')
+                if self.board[row][col] == 'X':
+                    self.agent_location = row, col
+
+    def move(self, direction):
+        if direction == 'UP':
+            new_location = self.agent_location[0] - 1, self.agent_location[1]
+        elif direction == 'DOWN':
+            new_location = self.agent_location[0] + 1, self.agent_location[1]
+        elif direction == 'LEFT':
+            new_location = self.agent_location[0], self.agent_location[1] - 1
+        elif direction == 'RIGHT':
+            new_location = self.agent_location[0], self.agent_location[1] + 1
+        else:
+            raise ValueError('Invalid move')
+
+        old_value = self.board[self.agent_location[0]][self.agent_location[1]]
+        self.board[self.agent_location[0]][self.agent_location[1]] = self.board[new_location[0]][new_location[1]]
+        self.board[new_location[0]][new_location[1]] = old_value
+        self.agent_location = new_location
+
+    def get_legal_move(self):
+        legal_moves = []
+        row, col = self.agent_location
+        if row != 0:
+            legal_moves.append('UP')
+        if row != self.size - 1:
+            legal_moves.append('DOWN')
+        if col != 0:
+            legal_moves.append('LEFT')
+        if col != self.size - 1:
+            legal_moves.append('RIGHT')
+        return legal_moves
+
+    def get_agent_location(self):
+        print(self.agent_location)
 
     def is_goal(self):
-        return True
+        if self.board[self.size - 3][self.size - 3] == 'A' and self.board[self.size - 2][self.size - 3] == 'B' and \
+                self.board[self.size - 1][self.size - 3] == 'C':
+            return True
+        else:
+            return False
 
     def create_board_string(self):
         lines = []
--- Runner.py
+++ Runner.py
@@ -1,4 +1,10 @@
+import os
+
 from BlocksWorld import BlocksWorld
+
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 start_state = {
     13: "A",
@@ -6,6 +12,27 @@ start_state = {
     15: "C",
     16: "X"
 }
-world = BlocksWorld(4, start_state)
 
-world.display_board()
+size = int(input('Please select board size: '))
+blocks_world = BlocksWorld(size, start_state)
+
+while not blocks_world.is_goal():
+    blocks_world.display_board()
+    print('Movable direction: ' + ', '.join(blocks_world.get_legal_move()))
+    user_input = input('Please input direction (w = up, a = left, s = down, d = right)\n')
+    direction = ''
+    if user_input == 'w':
+        direction = 'UP'
+    elif user_input == 'a':
+        direction = 'LEFT'
+    elif user_input == 's':
+        direction = 'DOWN'
+    elif user_input == 'd':
+        direction = 'RIGHT'
+
+    if direction != '':
+        blocks_world.move(direction)
+    cls()
+
+blocks_world.display_board()
+print("You have won")
